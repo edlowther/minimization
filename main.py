462935986b9cf0776dff5504c594c077c55241a3
edlowther <edlowther@users.noreply.github.com>
2024-06-18 13:43:49 +0100

    Remove hard-coded table in report

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import pandas as pd
 import numpy as np
+from yaml import safe_load
 from yaml.scanner import ScannerError
 
 from src.data_loader import DataLoader
@@ -10,7 +11,8 @@ from src.allocation_handler import AllocationHandler
 # Configure the project: 
 minimisation_weight = 0.8 # 0.0 for fully random allocation, 1.0 for no randomisation
 source_filename = './new-participant-data.yaml'
-expected_yaml_file_keys = ['participant_id', 'cancer_type', 'hospital_site']
+with (open(source_filename)) as f:
+    expected_yaml_file_keys = safe_load(f).keys()
 allocations_filepath = './allocations.csv'
 
 # The commit hash gets passed to this script via the GitLab runner and is useful to ensure
@@ -40,7 +42,6 @@ if data_loaded:
         print(data_loader.participant.error_message)
         report_builder.add_error(data_loader.participant.error_message)
         report_builder.analyse_metadata(data_loader.allocations_df)
-        report_builder.make_table(data_loader.allocations_df)
 
     # The code goes ahead and generates the report if the new data is valid:
     else: 
@@ -58,6 +59,5 @@ if data_loaded:
 
         report_builder.analyse_metadata(updated_allocations_df)
         report_builder.make_chart(updated_allocations_df, data_loader.participant.data.keys(), data_loader.subcategory_to_category_lookup)
-        report_builder.make_table(updated_allocations_df)
 
 report_builder.write_report()
